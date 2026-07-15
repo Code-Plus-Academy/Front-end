34a1ea9d5651257095cd42f1c711f8571c5a827f
atharvakapse831 <atharvakapse831@gmail.com>
2026-07-15 13:48:06 +0530

    design(settings): replace placeholder icons with actual brand SVG logos for social input prefixes

--- src/views/Settings.jsx
+++ src/views/Settings.jsx
@@ -177,6 +177,9 @@ const I = {
   code: "M16 18l6-6-6-6M8 6l-6 6 6 6",
   github: "M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22",
   link: "M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71",
+  linkedinBrand: "M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z M2 9h4v12H2z M4 2a2 2 0 1 0 0 4 2 2 0 0 0 0-4",
+  twitterBrand: "M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z",
+  youtubeBrand: "M22.54 6.42a2.78 2.78 0 0 0-1.94-2C18.88 4 12 4 12 4s-6.88 0-8.6.46a2.78 2.78 0 0 0-1.94 2A29 29 0 0 0 1 11.75a29 29 0 0 0 .46 5.33A2.78 2.78 0 0 0 3.4 19c1.72.46 8.6.46 8.6.46s6.88 0 8.6-.46a2.78 2.78 0 0 0 1.94-2 29 29 0 0 0 .46-5.25 29 29 0 0 0-.46-5.33z M9.75 15.02l5.75-3.27-5.75-3.27z",
   map: "M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0zM12 7a3 3 0 1 0 0 6 3 3 0 0 0 0-6z",
   info: "M12 22a10 10 0 1 0 0-20 10 10 0 0 0 0 20zM12 8h.01M11 12h1v4h1",
   folder: "M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z",
@@ -866,7 +869,7 @@ const [uChecking, setUChecking] = useState(false);
           <Field label="GitHub" t={t}>
             <Input value={form.github} onChange={v => setForm(f => ({ ...f, github: v }))} placeholder="@username" t={t} prefix={<Ic p={I.github} s={14} c={t.text2} />} />
           </Field>
-          {[["linkedin", "LinkedIn", I.link], ["twitter", "X / Twitter", I.msg], ["youtube", "YouTube", I.zap]].map(([k, l, icon]) => (
+          {[["linkedin", "LinkedIn", I.linkedinBrand], ["twitter", "X / Twitter", I.twitterBrand], ["youtube", "YouTube", I.youtubeBrand]].map(([k, l, icon]) => (
             <Field key={k} label={l} t={t}>
               <Input value={form[k]} onChange={v => setForm(f => ({ ...f, [k]: v }))} placeholder={`@${k}`} t={t} prefix={<Ic p={icon} s={14} c={t.text2} />} />
             </Field>
